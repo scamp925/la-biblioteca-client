3304d04e63fcff6939653d63d20f0a62bfb9b234
Sariah Campopiano <scampopiano925@gmail.com>
2023-02-11 11:08:56 -0600

    Changed RegisterForm.js to capture what is needed from new user

--- components/RegisterForm.js
+++ components/RegisterForm.js
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-unescaped-entities */
 import PropTypes from 'prop-types';
 import { useState } from 'react';
 import Button from 'react-bootstrap/Button';
@@ -5,8 +6,15 @@ import Form from 'react-bootstrap/Form';
 import { registerUser } from '../utils/auth'; // Update with path to registerUser
 
 function RegisterForm({ user, updateUser }) {
+  const [firstName, lastName] = user.fbUser.displayName.split(' ');
+  const date = new Date().toISOString().slice(0, 10);
+
   const [formData, setFormData] = useState({
-    bio: '',
+    firstName,
+    lastName,
+    profileImage: '',
+    nickname: '',
+    createdOn: date,
     uid: user.uid,
   });
 
@@ -16,22 +24,31 @@ function RegisterForm({ user, updateUser }) {
   };
 
   return (
-    <Form onSubmit={handleSubmit}>
-      <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Label>Gamer Bio</Form.Label>
-        <Form.Control as="textarea" name="bio" required placeholder="Enter your Bio" onChange={({ target }) => setFormData((prev) => ({ ...prev, [target.name]: target.value }))} />
-        <Form.Text className="text-muted">Let other gamers know a little bit about you...</Form.Text>
-      </Form.Group>
-      <Button variant="primary" type="submit">
-        Submit
-      </Button>
-    </Form>
+    <>
+      <h3>Complete Your Account Registration</h3>
+      <Form onSubmit={handleSubmit}>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label className="text">What's Your Nickname?</Form.Label>
+          <Form.Control name="nickname" required placeholder="e.g. Cool_Kats" onChange={({ target }) => setFormData((prev) => ({ ...prev, [target.name]: target.value }))} />
+          <Form.Text className="text-muted">***Username MUST be different from your first and last name</Form.Text>
+          <div />
+          <Form.Label className="text">Profile Image</Form.Label>
+          <Form.Control name="profileImage" required placeholder="Image URL" onChange={({ target }) => setFormData((prev) => ({ ...prev, [target.name]: target.value }))} />
+        </Form.Group>
+        <Button variant="dark" type="submit">
+          Register for Account
+        </Button>
+      </Form>
+    </>
   );
 }
 
 RegisterForm.propTypes = {
   user: PropTypes.shape({
     uid: PropTypes.string.isRequired,
+    fbUser: PropTypes.shape({
+      displayName: PropTypes.string,
+    }).isRequired,
   }).isRequired,
   updateUser: PropTypes.func.isRequired,
 };
