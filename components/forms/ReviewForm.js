22c259aff65e3cb795b69685540dde80a21ec128
Sariah Campopiano <scampopiano925@gmail.com>
2023-02-13 20:44:45 -0600

    Created form for reviews and have the component invoked on a page component called new.js. Need to display book display on page

---
+++ components/forms/ReviewForm.js
@@ -0,0 +1,83 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import { useRouter } from 'next/router';
+import Form from 'react-bootstrap/Form';
+import { Button } from 'react-bootstrap';
+import { Rating } from 'react-simple-star-rating';
+import { useAuth } from '../../utils/context/authContext';
+import { createReview } from '../../utils/data/reviewData';
+
+function ReviewForm({ bookObj }) {
+  const [formInput, setFormInput] = useState({
+    starRating: 0,
+    content: '',
+    createdOn: '',
+    bookId: 0,
+    customerId: 0,
+  });
+  const { user } = useAuth();
+  const router = useRouter();
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormInput((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
+
+  const handleRating = (e) => {
+    const value = e;
+    setFormInput((prevState) => ({
+      ...prevState,
+      starRating: value,
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    createReview(bookObj.id, user, formInput).then(() => {
+      router.push(`/books/${bookObj.id}`);
+    });
+  };
+
+  return (
+    <div>
+      <Form onSubmit={handleSubmit}>
+        <div className="event-form-star-rating-div">
+          <Rating
+            allowHover={false}
+            size={50}
+            allowHalfIcon
+            ratingValue={formInput.starRating}
+            onClick={handleRating}
+          />
+        </div>
+        <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+          <Form.Control as="textarea" rows={3} type="text" placeholder="Write a review" name="content" value={formInput.content} onChange={handleChange} />
+        </Form.Group>
+        <Button type="submit" variant="success">Add My Review</Button>
+        {/* <div className="form-btn">
+          <Button type="submit" variant="success">{reviewObj?.id ? 'Update' : 'Add'} My Review</Button>
+        </div> */}
+      </Form>
+    </div>
+  );
+}
+
+ReviewForm.propTypes = {
+  reviewObj: PropTypes.shape({
+    id: PropTypes.number,
+    starRating: PropTypes.string,
+    content: PropTypes.string,
+    createdOn: PropTypes.string,
+    user: PropTypes.shape({
+      id: PropTypes.number,
+    }),
+  }).isRequired,
+  bookObj: PropTypes.shape({
+    id: PropTypes.number,
+  }).isRequired,
+};
+
+export default ReviewForm;
---
+++ pages/reviews/new.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReviewForm from '../../components/forms/ReviewForm';
+
+export default function PaymentTypeForm() {
+  return (
+    <div>
+      <ReviewForm />
+    </div>
+  );
+}
