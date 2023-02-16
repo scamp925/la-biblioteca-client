a1c3beb172b2f23732086d710dc755c1edd057f5
Sariah Campopiano <scampopiano925@gmail.com>
2023-02-15 22:13:25 -0600

    Fixed a bug => terinary for adding or editing a review was stuck on 'edit your review' for every book. That shouldn't be the case. Add more logic to the terinary to filter it properly and bug solved

--- components/cards/SingleBookCard.js
+++ components/cards/SingleBookCard.js
@@ -3,8 +3,10 @@ import PropTypes from 'prop-types';
 import Image from 'react-bootstrap/Image';
 import Link from 'next/link';
 import Button from 'react-bootstrap/Button';
+import { useAuth } from '../../utils/context/authContext';
 
 function SingleBookCard({ bookObj, reviewObj }) {
+  const { user } = useAuth();
   const date = new Date(bookObj.firstPublished);
   const dateOptions = { year: 'numeric', month: 'long', day: 'numeric' };
   return (
@@ -16,7 +18,7 @@ function SingleBookCard({ bookObj, reviewObj }) {
           <Button variant="success">{bookObj.bookShelf ? 'Move to Another Shelf' : 'Add to My Books'}</Button>
         </Link>
         <Link passHref href={reviewObj ? `/reviews/edit/${reviewObj.id}` : `/reviews/new/${bookObj.id}`}>
-          <Button variant="outline-success">{reviewObj ? 'Edit Your Review' : 'Rate This Book'}</Button>
+          <Button variant="outline-success">{reviewObj.user?.id === user.id ? 'Edit Your Review' : 'Rate This Book'}</Button>
         </Link>
       </aside>
       <main>
@@ -43,6 +45,9 @@ SingleBookCard.propTypes = {
   }).isRequired,
   reviewObj: PropTypes.shape({
     id: PropTypes.number,
+    user: PropTypes.shape({
+      id: PropTypes.number,
+    }),
   }),
 };
 
