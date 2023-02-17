397cc066bfb6ad5c1754215e7900bdff0492d3b4
Sariah Campopiano <scampopiano925@gmail.com>
2023-02-16 22:24:59 -0600

    On SingleBookCard, updated the route to BookShelfForm. On the dynamic page, getting the book snippet info and BookShelfForm their needed info

--- pages/books/bookshelves/new/[id].js
+++ pages/books/bookshelves/new/[id].js
@@ -1,9 +1,33 @@
-import React from 'react';
+/* eslint-disable react-hooks/exhaustive-deps */
+import { useRouter } from 'next/router';
+import React, { useEffect, useState } from 'react';
+import BookSnippetCard from '../../../../components/cards/BookSnippetCard';
+import BookShelfForm from '../../../../components/forms/BookShelfForm';
+import { useAuth } from '../../../../utils/context/authContext';
+import { getSingleBook } from '../../../../utils/data/bookData';
 
 export default function AddToShelfForm() {
+  const router = useRouter();
+  const { id } = router.query;
+  const { user } = useAuth();
+  const [book, setBook] = useState({});
+
+  const singleBook = () => {
+    getSingleBook(id, user.id).then(setBook);
+  };
+
+  useEffect(() => {
+    singleBook();
+  }, [id]);
+
   return (
     <div>
-      Pass
+      <section>
+        <BookSnippetCard bookObj={book} />
+      </section>
+      <section>
+        <BookShelfForm bookId={id} />
+      </section>
     </div>
   );
 }
