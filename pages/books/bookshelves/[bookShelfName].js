11c55cabcf9585b769bd26e4ef058f225d372840
Sariah Campopiano <scampopiano925@gmail.com>
2023-02-17 21:08:01 -0600

    Created dynamic page called bookShelfName. With the help of the filter method and conditions, the books the user put on each shelf will display on the shelf 'page'. i.e. All books marked as read will render on the read page

---
+++ pages/books/bookshelves/[bookShelfName].js
@@ -0,0 +1,73 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import { useRouter } from 'next/router';
+import React, { useEffect, useState } from 'react';
+import BookCard from '../../../components/cards/BookCard';
+import { useAuth } from '../../../utils/context/authContext';
+import { getAllUserBooks } from '../../../utils/data/bookData';
+
+export default function BookShelves() {
+  const router = useRouter();
+  const { bookShelfName } = router.query;
+  const { user } = useAuth();
+  const [books, setBooks] = useState([]);
+
+  const allBooks = () => {
+    getAllUserBooks(user.id).then(setBooks);
+  };
+
+  const booksOnWantToRead = books.filter((book) => book.book_shelf === 'Want to Read');
+
+  const booksOnCurrentlyReading = books.filter((book) => book.book_shelf === 'Currently Reading');
+
+  const booksOnRead = books.filter((book) => book.book_shelf === 'Read');
+
+  useEffect(() => {
+    allBooks();
+  }, [bookShelfName, user]);
+
+  if (bookShelfName === 'wantToRead') {
+    return (
+      <div>
+        <header>
+          <h3>The Worlds Waiting For You</h3>
+          <h2>Want To Read Shelf</h2>
+        </header>
+        <div>
+          {booksOnWantToRead?.map((book) => (
+            <BookCard key={book.id} bookObj={book} />
+          ))}
+        </div>
+      </div>
+    );
+  }
+  if (bookShelfName === 'currentlyReading') {
+    return (
+      <div>
+        <header>
+          <h3>The Worlds Waiting For You</h3>
+          <h2>Want To Read Shelf</h2>
+        </header>
+        <div>
+          {booksOnCurrentlyReading?.map((book) => (
+            <BookCard key={book.id} bookObj={book} />
+          ))}
+        </div>
+      </div>
+    );
+  }
+  if (bookShelfName === 'read') {
+    return (
+      <div>
+        <header>
+          <h3>The Worlds Waiting For You</h3>
+          <h2>Want To Read Shelf</h2>
+        </header>
+        <div>
+          {booksOnRead?.map((book) => (
+            <BookCard key={book.id} bookObj={book} />
+          ))}
+        </div>
+      </div>
+    );
+  }
+}
