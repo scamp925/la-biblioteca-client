84ef3b142c810f73c3cb7dcea7f81eca6d3c3e3a
Sariah Campopiano <scampopiano925@gmail.com>
2023-02-18 13:24:52 -0600

    Since API call to getSingleBook changed, I had to update date the function that calls it on reviews/new/book_id

--- pages/reviews/new/[id].js
+++ pages/reviews/new/[id].js
@@ -4,14 +4,16 @@ import { useRouter } from 'next/router';
 import ReviewForm from '../../../components/forms/ReviewForm';
 import { getSingleBook } from '../../../utils/data/bookData';
 import BookSnippetCard from '../../../components/cards/BookSnippetCard';
+import { useAuth } from '../../../utils/context/authContext';
 
 export default function CreateReviewForm() {
   const router = useRouter();
   const { id } = router.query;
+  const { user } = useAuth();
   const [book, setBook] = useState({});
 
   const singleBook = () => {
-    getSingleBook(id).then(setBook);
+    getSingleBook(id, user.id).then(setBook);
   };
 
   useEffect(() => {
