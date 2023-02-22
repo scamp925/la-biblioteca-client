97f202517a87c32c2fef275c9f8e48ec6b23380b
Sariah Campopiano <scampopiano925@gmail.com>
2023-02-21 19:47:17 -0600

    Got the search bar div looking the way I would like it

--- components/Search.js
+++ components/Search.js
@@ -21,22 +21,25 @@ function Search({ books, setFilteredBooks }) {
 
   return (
     <div>
-      <h5 className="center-text">Search and Browse Books</h5>
-      <div className="search">
-        <InputGroup className="mb-3">
-          <Form.Control
-            placeholder="Title/Author"
-            aria-label="Title/Autho"
-            value={searchInput}
-            onChange={handleChange}
-            aria-describedby="basic-addon2"
-          />
-          <Button variant="secondary" onClick={resetInput}>
-            Reset Search
-          </Button>
-        </InputGroup>
+      <div className="search-text">
+        <h5>Search and Browse Books</h5>
       </div>
-
+      <div className="search">
+        <div>
+          <InputGroup className="mb-3">
+            <Form.Control
+              placeholder="Title/Author"
+              aria-label="Title/Autho"
+              value={searchInput}
+              onChange={handleChange}
+              aria-describedby="basic-addon2"
+            />
+            <Button variant="secondary" onClick={resetInput}>
+              Reset Search
+            </Button>
+          </InputGroup>
+        </div>
+      </div>
     </div>
   );
 }
